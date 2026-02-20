9bf49f9c7c187675dae36d3d4eec396d153346c7
Piyush Jain <piyushjainwork01@gmail.com>
2026-02-20 11:28:34 +0530

    Initial Spring Boot Kafka project setup

---
+++ kafka-ecommerce-poc/src/main/java/com/example/kafkaecommerce/KafkaEcommercePocApplication.java
@@ -0,0 +1,13 @@
+package com.example.kafkaecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaEcommercePocApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaEcommercePocApplication.class, args);
+    }
+
+}
---
+++ kafka-ecommerce-poc/src/test/java/com/example/kafkaecommerce/KafkaEcommercePocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kafkaecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaEcommercePocApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
